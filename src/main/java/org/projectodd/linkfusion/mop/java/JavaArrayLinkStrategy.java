f0ab6cc9c3e0a3f9f9a464b5898c2e5e2dc7e56f
Bob McWhirter <bob@mcwhirter.org>
2013-01-18 11:47:10 -0500

    Array-element setting linkage.

--- src/main/java/org/projectodd/linkfusion/mop/java/JavaArrayLinkStrategy.java
+++ src/main/java/org/projectodd/linkfusion/mop/java/JavaArrayLinkStrategy.java
@@ -48,32 +48,26 @@ public class JavaArrayLinkStrategy extends NonContextualLinkStrategy {
         return lookup().findStatic(Integer.class, "parseInt", methodType(int.class, String.class));
     }
 
-    /*
-     * @Override
-     * public StrategicLink linkSetProperty(StrategyChain chain, Object receiver, String propName, Object value, Binder binder, Binder guardBinder)
-     * throws NoSuchMethodException, IllegalAccessException {
-     * 
-     * Resolver resolver = getResolver(receiver.getClass());
-     * DynamicMethod dynamicWriter = resolver.getInstanceResolver().getPropertyWriter(propName);
-     * 
-     * if (dynamicWriter == null) {
-     * return chain.nextStrategy();
-     * }
-     * 
-     * // MethodHandle method = dynamicWriter.findMethodHandle(new Object[] { value });
-     * InvocationPlan plan = dynamicWriter.findMethodInvoationPlan(new Object[] { value });
-     * 
-     * if (plan == null) {
-     * return chain.nextStrategy();
-     * }
-     * 
-     * MethodHandle method = binder
-     * .drop(1)
-     * .filter(1, plan.getFilters())
-     * .invoke(plan.getMethodHandle());
-     * 
-     * return new StrategicLink(method, getReceiverClassAndNameGuard(receiver.getClass(), propName, guardBinder));
-     * }
-     */
+    @Override
+    public StrategicLink linkSetProperty(StrategyChain chain, Object receiver, String propName, Object value, Binder binder, Binder guardBinder)
+            throws NoSuchMethodException, IllegalAccessException {
+        
+        if (!receiver.getClass().isArray()) {
+            return chain.nextStrategy();
+        }
+
+        try {
+            int index = Integer.parseInt(propName);
+            MethodHandle method = binder
+                    .filter(1, arrayIndexFilter())
+                    .invoke(lookup().findStatic(Array.class, "set", methodType(void.class, Object.class, int.class, Object.class)));
+            return new StrategicLink(method, getReceiverClassAndNameGuard(receiver.getClass(), propName, guardBinder));
+
+        } catch (NumberFormatException e) {
+            // ignore
+        }
+
+        return chain.nextStrategy();
+    }
 
 }
